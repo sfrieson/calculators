acbd10a1252bcaacd27267f74e7ae141d190c349
Steven Frieson <sfrieson@gmail.com>
2019-01-27 09:46:53 -0500

    Adds basic Vue app.

---
+++ browser/build/webpack.config.dev.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+const { VueLoaderPlugin } = require('vue-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: ['./src/app.js'],
+  devServer: {
+    hot: true,
+    watchOptions: {
+      poll: true
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        exclude: /node_modules/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    })
+  ]
+};
---
+++ browser/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="full-width center-content">
+    <hello-component name="Amy" />
+  </div>
+</template>
+
+<script>
+import HelloComponent from './components/HelloComponent.vue'
+export default {
+  components: {
+    HelloComponent
+  }
+}
+</script>
+
